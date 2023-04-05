0bb078901a7a62ab8aff1c1d7ee5e7c7bebe36cd
Anna Kostina <anna@kostina.xyz>
2023-04-05 19:32:49 +0000

    draft of the etls

---
+++ 02_prefect/etl_web_to_gcs.py
@@ -0,0 +1,72 @@
+from pathlib import Path
+import pandas as pd
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
+from random import randint
+from prefect.tasks import task_input_hash
+from datetime import timedelta
+
+
+@task(retries=3, cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
+def fetch(dataset_url: str) -> pd.DataFrame:
+    """Read taxi data from web into pandas DataFrame"""
+
+    df = pd.read_csv(dataset_url)
+    return df
+
+
+@task(log_prints=True)
+def clean(df: pd.DataFrame) -> pd.DataFrame:
+    """Fix dtype issues"""
+    df["pickup_datetime"] = pd.to_datetime(df["pickup_datetime"])
+    df["dropoff_datetime"] = pd.to_datetime(df["dropoff_datetime"])
+    df["DOlocationID"] = df["DOlocationID"].astype("Int64")
+    df["PUlocationID"] = df["PUlocationID"].astype("Int64")
+    df["SR_Flag"] = df["SR_Flag"].astype("Int64")
+
+    print(df.head(2))
+    print(f"columns: {df.dtypes}")
+    print(f"rows: {len(df)}")
+    return df
+
+
+@task()
+def write_local(df: pd.DataFrame, coin: str, dataset_file: str) -> Path:
+    """Write DataFrame out locally as parquet file"""
+    path = Path(f"data//{dataset_file}.parquet")
+    df.to_parquet(path, compression="gzip")
+    return path
+
+
+@task()
+def write_gcs(path: Path) -> None:
+    """Upload local parquet file to GCS"""
+    gcs_block = GcsBucket.load("zoomcamp-gcs")
+    gcs_block.upload_from_path(from_path=f"{path}", to_path=path)
+    return
+
+
+@flow()
+def etl_web_to_gcs(year: int, month: int) -> None:
+    """The main ETL function"""
+    coin = "fhv_parquet"
+    dataset_file = f"{coin}_tripdata_{year}-{month:02}"
+    dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/fhv/{dataset_file}.csv.gz"
+
+    df = fetch(dataset_url)
+    df_clean = clean(df)
+    path = write_local(df_clean, coin, dataset_file)
+    write_gcs(path)
+
+@flow()
+def etl_parent_flow(
+    months: list[int] = [1,2,3], year: int = 2020
+):
+    for month in months:
+        etl_web_to_gcs(year, month)
+
+if __name__ == "__main__":
+    months = [1,2,3]
+    #,4,5,6,7,8,9,10,11,12
+    year = 2020
+    etl_parent_flow(months, year)
